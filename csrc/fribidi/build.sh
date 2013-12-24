f00c59e9c82bdea63bd5bb44a9c6b482c111a0b9
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:42:43 +0200

    changed binary paths in build scripts

--- csrc/fribidi/build.sh
+++ csrc/fribidi/build.sh
@@ -1,4 +1,4 @@
-[[ "$(uname)" == *Linux* ]] && libfile=../../linux/bin/libfribidi.so
-[[ "$(uname)" == *MINGW* ]] && libfile=../../bin/fribidi.dll
+[[ "$(uname)" == *Linux* ]] && libfile=../../bin/linux32/libfribidi.so
+[[ "$(uname)" == *MINGW* ]] && libfile=../../bin/mingw32/fribidi.dll
 
-gcc -shared -O3 -s -o "$libfile" -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H src/*.c src/charset/*.c
+gcc src/*.c src/charset/*.c -shared -O3 -s -o "$libfile" -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
