94fefe9994b7ee98c5583dc127b153192e1ef6f0
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:50:28 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/fribidi/build-linux32.sh
+++ csrc/fribidi/build-linux32.sh
@@ -1,1 +1,1 @@
-gcc $CFLAGS src/*.c src/charset/*.c -shared -o ../../bin/linux32/libfribidi.so -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
+gcc -O2 -s -static-libgcc src/*.c src/charset/*.c -shared -o ../../bin/linux32/libfribidi.so -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
