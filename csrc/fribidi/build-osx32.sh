6ffea71c442f918d710dbb75297ab0806726cb6a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:02 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/fribidi/build-osx32.sh
+++ csrc/fribidi/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 src/*.c src/charset/*.c -shared -o ../../bin/osx32/libfribidi.dylib -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
+gcc -arch i386 -O2 src/*.c src/charset/*.c -shared -install_name @loader_path/libfribidi.dylib -o ../../bin/osx32/libfribidi.dylib -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
--- csrc/fribidi/build-osx64.sh
+++ csrc/fribidi/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 src/*.c src/charset/*.c -shared -o ../../bin/osx64/libfribidi.dylib -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
+gcc -arch x86_64 -O2 src/*.c src/charset/*.c -shared -install_name @loader_path/libfribidi.dylib -o ../../bin/osx64/libfribidi.dylib -Isrc -Isrc/charset -Wall -ansi -DHAVE_CONFIG_H
