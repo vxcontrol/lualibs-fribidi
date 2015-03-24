c82d4d84c8d0ad3b8b364c122e5edf5bd6668534
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:58:40 +0200

    unimportant

--- csrc/fribidi/build-linux64.sh
+++ csrc/fribidi/build-linux64.sh
@@ -1,1 +1,1 @@
-P=linux64 C=-fPIC L="-s -static-libgcc" D=libfribidi.so A=libfribidi.a ./build.sh
+P=linux64 C="-fPIC -include _memcpy.h" L="-s -static-libgcc" D=libfribidi.so A=libfribidi.a ./build.sh
